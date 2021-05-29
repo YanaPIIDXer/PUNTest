a6fdb4dd8e28827c55ef00f34cb939929d831e35
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 16:11:51 +0900

    ゲームプレイフロークラスを定義 開始時にプレイヤーを生成するようにした

---
+++ Assets/Scripts/Flow/GameplayFlow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Game.Flow
+{
+    /// <summary>
+    /// ゲームプレイフロー
+    /// </summary>
+    public class GameplayFlow : MonoBehaviourPunCallbacks
+    {
+        void Awake()
+        {
+            PhotonNetwork.Instantiate("Prefabs/Player", Vector3.zero, Quaternion.identity);
+        }
+    }
+}
