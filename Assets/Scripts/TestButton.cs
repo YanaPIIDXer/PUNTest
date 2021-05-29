4ab90bd43880db5f34762719f47abadf96bd0c8f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 14:24:22 +0900

    ボタン押下で接続するようにした

--- Assets/Scripts/TestButton.cs
+++ Assets/Scripts/TestButton.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using UnityEngine.UI;
+using Network;
 
 /// <summary>
 /// 実験用ボタン
@@ -8,4 +13,16 @@ using UnityEngine;
 /// </summary>
 public class TestButton : MonoBehaviour
 {
+    /// <summary>
+    /// 接続オブジェクト
+    /// </summary>
+    [SerializeField]
+    private Connection Conn = null;
+
+    void Awake()
+    {
+        GetComponent<Button>().OnClickAsObservable()
+            .Subscribe((_) => Conn.Connect())
+            .AddTo(gameObject);
+    }
 }
