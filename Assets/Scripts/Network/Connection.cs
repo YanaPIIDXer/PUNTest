b12ea5b3ce21cfe82f45608e763f7da17df6ac0d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 14:07:54 +0900

    接続用クラスをとりあえず定義

---
+++ Assets/Scripts/Network/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Network
+{
+    /// <summary>
+    /// 接続クラス
+    /// </summary>
+    public class Connection
+    {
+        /// <summary>
+        /// 接続
+        /// </summary>
+        public void Connect()
+        {
+        }
+
+        #region Singleton
+        public static Connection Instance { get { return _Instance; } }
+        private static Connection _Instance = new Connection();
+        private Connection() { }
+        #endregion
+    }
+}
