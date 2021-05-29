4aad722fcc7230c9b22b2c6638c575bdf7de8552
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 15:11:02 +0900

    ゲームフロー管理クラス定義

---
+++ Assets/Scripts/Flow/GameFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Game.Flow
+{
+    /// <summary>
+    /// ゲームフロー管理クラス
+    /// </summary>
+    public class GameFlow : MonoBehaviourPunCallbacks
+    {
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("On Connected!");
+        }
+    }
+}
