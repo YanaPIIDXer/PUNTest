51eb5816a094e39949e09e21c115baaa2fbca2d0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-30 18:12:18 +0900

    ダウンロードしたテクスチャをストリーミングで送受信してマテリアルに反映する処理を実装

---
+++ Assets/Scripts/Player/ImageReceiver.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+namespace Game.Player
+{
+    public class ImageReceiver : MonoBehaviour, IOnEventCallback
+    {
+        /// <summary>
+        /// PhotonView
+        /// </summary>
+        private PhotonView View = null;
+
+        /// <summary>
+        /// レンダラ
+        /// </summary>
+        private MeshRenderer Renderer = null;
+
+        /// <summary>
+        /// テクスチャの幅
+        /// </summary>
+        private int TextureWidth = 0;
+
+        /// <summary>
+        /// テクスチャの高さ
+        /// </summary>
+        private int TextureHeight = 0;
+
+        /// <summary>
+        /// 受信バッファ
+        /// </summary>
+        private byte[] RecvBuffer = null;
+
+        /// <summary>
+        /// 受信済みのデータ長
+        /// </summary>
+        private int CurrentLength = 0;
+
+        /// <summary>
+        /// ストリーミング受信中？
+        /// </summary>
+        private bool bIsStreaming = false;
+
+        void Awake()
+        {
+            View = GetComponent<PhotonView>();
+            Renderer = GetComponent<MeshRenderer>();
+        }
+
+        void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        public void OnEvent(EventData photonEvent)
+        {
+            switch (photonEvent.Code)
+            {
+                case ImageSender.BeginStreamEventCode:
+
+                    BeginStream(photonEvent.CustomData as int[]);
+                    break;
+
+                case ImageSender.StreamingEventCode:
+
+                    if (bIsStreaming)
+                    {
+                        RecvStream(photonEvent.CustomData as byte[]);
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// ストリーム開始
+        /// </summary>
+        /// <param name="Info">情報</param>
+        private void BeginStream(int[] Info)
+        {
+            if (View.ViewID != Info[0]) { return; }
+
+            TextureWidth = Info[1];
+            TextureHeight = Info[2];
+            RecvBuffer = new byte[Info[3]];
+            CurrentLength = 0;
+
+            bIsStreaming = true;
+        }
+
+        /// <summary>
+        /// ストリーム受信
+        /// </summary>
+        /// <param name="Data">データ</param>
+        private void RecvStream(byte[] Data)
+        {
+            Data.CopyTo(RecvBuffer, CurrentLength);
+            CurrentLength += Data.Length;
+            if (CurrentLength >= RecvBuffer.Length)
+            {
+                bIsStreaming = false;
+
+                Texture2D Tex = new Texture2D(TextureWidth, TextureHeight, TextureFormat.RGB24, false);
+                Tex.LoadRawTextureData(RecvBuffer);
+                Tex.Apply();
+
+                Renderer.material.mainTexture = Tex;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ImageSender.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using Photon.Pun;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+namespace Game.Player
+{
+    /// <summary>
+    /// 画像送信
+    /// </summary>
+    public class ImageSender : MonoBehaviour
+    {
+        /// <summary>
+        /// PhotonView
+        /// </summary>
+        private PhotonView View = null;
+
+        /// <summary>
+        /// ストリーム開始イベントコード
+        /// </summary>
+        public const int BeginStreamEventCode = 10;
+
+        /// <summary>
+        /// ストリーミングイベントコード
+        /// </summary>
+        public const int StreamingEventCode = 11;
+
+        /// <summary>
+        /// ストリーム送信中？
+        /// </summary>
+        private bool bIsStreaming = false;
+
+        void Awake()
+        {
+            View = GetComponent<PhotonView>();
+            this.UpdateAsObservable()
+                .Where(_ => View.IsMine && Input.GetKeyDown(KeyCode.I) && !bIsStreaming)
+                .Subscribe(_ => Execute());
+        }
+
+        /// <summary>
+        /// 実行
+        /// </summary>
+        private async void Execute()
+        {
+            var DownloadedTexture = await DownloadImage();
+
+            int Width = DownloadedTexture.width;
+            int Height = DownloadedTexture.height;
+            byte[] RawData = DownloadedTexture.GetRawTextureData();
+            int DataLength = RawData.Length;
+
+            int[] TexInfo = new int[] { View.ViewID, Width, Height, DataLength };
+
+            RaiseEventOptions EventOps = new RaiseEventOptions
+            {
+                CachingOption = EventCaching.DoNotCache,
+                Receivers = ReceiverGroup.All,
+            };
+
+            SendOptions SendOps = new SendOptions
+            {
+                Reliability = true,
+            };
+            PhotonNetwork.RaiseEvent(BeginStreamEventCode, TexInfo, EventOps, SendOps);
+
+            bIsStreaming = true;
+            RawData.ToObservable()
+                   .Buffer(1024 * 5)
+                   .Subscribe(async (Data) =>
+                   {
+                       byte[] SendData = new byte[Data.Count];
+                       Data.CopyTo(SendData, 0);
+                       PhotonNetwork.RaiseEvent(StreamingEventCode, SendData, EventOps, SendOps);
+                       await UniTask.Delay(10);
+                   }).AddTo(gameObject);
+            bIsStreaming = false;
+        }
+
+        /// <summary>
+        /// 画像のダウンロード
+        /// </summary>
+        /// <returns>テクスチャ</returns>
+        private async UniTask<Texture2D> DownloadImage()
+        {
+            var Request = UnityWebRequestTexture.GetTexture("https://pbs.twimg.com/profile_images/1181428975328841729/Jaqmb94s_400x400.jpg");
+            await Request.SendWebRequest();
+            return DownloadHandlerTexture.GetContent(Request);
+        }
+
+        public void OnEvent(EventData photonEvent)
+        {
+            switch (photonEvent.Code)
+            {
+                case 10:
+
+                    Debug.Log("Start Streaming.");
+                    break;
+            }
+        }
+    }
+}
